23c76b4f9458fd5ed0c7e8ca52972753fcdd97cd
Avinash Varna <avinash.varna@gmail.com>
2018-02-15 21:32:16 -0700

    Updated version for pypi release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sanskrit_util',
-    version='0.1.1.dev1',
+    version='0.1.1.dev22,
     description='Sanskrit utilities',
     long_description='Collection of utilities for analyzing and working with Sanskrit',
 
